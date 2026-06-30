37ecce8ee511fa2dd5867201fff8d84895acd547
Cesar <cessardevs@gmail.com>
2026-06-30 16:00:07 +0100

    basics

--- basics.ts
+++ basics.ts
@@ -1,3 +1,16 @@
 let userName: string;
 let userAge = 22;
 
+//..
+
+userName = "Cesar";
+// userAge = 18;
+
+function add(a: number, b = 5) {
+  return a + b;
+}
+
+add(10);
+// add("10") --> Argument of type 'string' is not assignable to parameter of type 'number'.
+add(10, 6);
+// add(10, "6") --> Argument of type 'string' is not assignable to parameter of type 'number'.
